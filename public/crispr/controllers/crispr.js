186517a2550ba457b3c5cabd05fa65b6b6301004
H.M. van Rossum <hmvanrossum@x082043.tnw-m.tudelft.nl>
2015-07-22 13:43:23 +0200

    Added some comments

--- public/crispr/controllers/crispr.js
+++ public/crispr/controllers/crispr.js
@@ -14,6 +14,9 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
         $scope.loci_fetched = [];
         $scope.loci_failed = [];
 
+        //method 1 is the single gRNA method
+        //method 2 is the double gRNA method
+        //for more information see our paper
         $scope.method1_presequence = 'TGCGCATGTTTCGGCGTTCGAAACTTCTCCGCAGTGAAAGATAAATGATC';
         $scope.method1_postsequence = 'GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAAC';
         $scope.method2_presequence = 'TGCGCATGTTTCGGCGTTCGAAACTTCTCCGCAGTGAAAGATAAATGATC';
@@ -28,6 +31,7 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
                 ranking_secondary_structure : $scope.global.user.ranking_secondary_structure
             };
         }else{
+            //default settings
             $scope.form = {
                 crispr_method : 1,
                 show_diagnostic_primers : true,
@@ -37,13 +41,12 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
             };
         }
         $scope.form.loci_list = '';
+
         $rootScope.$on('loggedin', function() {
             $scope.global.user  =  $rootScope.user;
         });
         
         $scope.find_strains = function() {
-           // var user = ($scope.global.user ? $scope.global.user : ($rootScope.user ? $rootScope.user : null));
-            console.log('find_strains User',$scope.global.user);
             Strains.query(function(strains) {
                 $scope.strains = strains;
                 if($scope.global.user){
@@ -57,11 +60,15 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
             $scope.loci_fetching = $scope.form.loci_list.match(/([\d\-A-z])+(,\d+)?/g).map(function(x) {
                 return x.toUpperCase();
             });
-            //only unqiue values
+            //only unqiue symbols
             $scope.loci_fetching = $scope.loci_fetching.filter(function(v,i) { return $scope.loci_fetching.indexOf(v) === i; });
             $scope.loading = true;
+
+            //array for the succesfully loaded loci
             $scope.loci = [];
+            //array with symbols of the loci that has been successfully fetched
             $scope.loci_fetched = [];
+            //array with symbols of the loci that could not be fetched
             $scope.loci_failed = [];
             $scope.errors = false;
             if($scope.loci_fetching.length > 0){
@@ -71,23 +78,27 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
                         locus: locus,
                         strain: $scope.form.strain
                     }, function(el) {
-                        console.log(el);  
+                        //if the locus could not be found, it doesn't have the _id property
                         if(el.hasOwnProperty('_id')){
                             el.display_name = locus;
                             if(!$scope.errors && el.hasOwnProperty('error_msg') && el.error_msg.length > 0){
                                 $scope.errors = true;
                             }
                             $scope.loci.push(el);
-                            $scope.findSome();
+                            $scope.findRestrictionSites(el);
                             $scope.loci_fetched.push(locus);
                         }else{
                             $scope.loci_failed.push(locus);
                         }
+
+                        //after the first locus has been fetched go to the result tab page
                         if(($scope.loci_fetched.length + $scope.loci_failed.length) === 1){
                             $scope.tabs[2].active = true;
-                            //go to result tab page
                         }
+
+                        //remove symbol from loci_fetching array
                         $scope.loci_fetching = $filter('filter')($scope.loci_fetching, '!'+locus);
+                        //if the array length is 0 we have fetched all the loci
                         if($scope.loci_fetching.length === 0){
                             $scope.loading = false;
                         }
@@ -138,16 +149,13 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
             });
         };
 
-   /*     $scope.find = function() {
-            Loci.query(function(loci) {
-                $scope.loci = loci;
-            });
-        };*/
 
         $scope.$watchGroup(['form.ranking_restriction_sites', 'form.ranking_gc_content','form.ranking_secondary_structure'], function() {
             $scope.updateRanking();
         });
 
+        //currently updates the ranking of all the fetched loci
+        //could be written a bit more efficient to only update the one that has just been fetched
         $scope.updateRanking = function(){
             $scope.loci.forEach(function(el) {
                 var i =0;
@@ -201,18 +209,18 @@ angular.module('mean.crispr').controller('CrisprController', ['$scope', '$rootSc
             return sequence.split('').reverse().join('');
         };
 
-        $scope.findSome = function() {
-            $scope.loci.forEach(function(locus){
-                locus.targets.forEach(function(target){
-                  target.enzymes = [];
-                    if(!!$scope.global.user && $scope.global.user.hasOwnProperty('restriction_enzymes') && $scope.global.user.restriction_enzymes instanceof Array){
-                        $scope.global.user.restriction_enzymes.forEach(function(enzyme) {
-                            if (RestrictionEnzymes.hasRestrictionSite(target.sequence_wo_pam, enzyme)) {
-                                target.enzymes.push(enzyme);
-                            }
-                        });
-                    }
-                });
+        //first finds restriction sites in the target sequences of the locus
+        //then updates the ranking
+        $scope.findRestrictionSites = function(locus) {
+             locus.targets.forEach(function(target){
+              target.enzymes = [];
+                if(!!$scope.global.user && $scope.global.user.hasOwnProperty('restriction_enzymes') && $scope.global.user.restriction_enzymes instanceof Array){
+                    $scope.global.user.restriction_enzymes.forEach(function(enzyme) {
+                        if (RestrictionEnzymes.hasRestrictionSite(target.sequence_wo_pam, enzyme)) {
+                            target.enzymes.push(enzyme);
+                        }
+                    });
+                }
             });
             $scope.updateRanking();
         };
